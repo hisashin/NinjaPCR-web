bdb60d9d5702186aaef319a7e81b58247af5af68
Jenkins-tori <jenkins@tori.st>
2018-06-07 14:15:38 +0900

    test

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp'); 
+var pug = require('gulp-pug');
+
+gulp.task('pug', () => {
+  return gulp.src(['./pug/**/*.pug', './pug/**/*.jade', '!./pug/**/_*.pug'])
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('./production/'));
+});
+
