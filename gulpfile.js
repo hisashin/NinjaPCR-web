5843601cbee2cac5f272a07bb34295b5edc94840
Jenkins-tori <jenkins@tori.st>
2018-06-07 14:27:16 +0900

    update guilpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'); 
 var pug = require('gulp-pug');
 
-gulp.task('pug', () => {
+gulp.task('pug', function() {
   return gulp.src(['./pug/**/*.pug', './pug/**/*.jade', '!./pug/**/_*.pug'])
   .pipe(pug({
     pretty: true
