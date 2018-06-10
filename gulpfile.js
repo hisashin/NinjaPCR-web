ca6e7c9f16f689c6664e68f2b26540095a3dfd08
Shingo Hisakawa <shingohisakawa@gmail.com>
2018-06-10 14:40:31 +0900

    pug&pugI18n

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
 var gulp = require('gulp'); 
-var pug = require('gulp-i18n-pug');
+var pug = require('gulp-pug');
+var pugI18n = require('gulp-i18n-pug');
 
+gulp.task('default', ['pug', 'pugI18n']);
 gulp.task('pug', function() {
+  return gulp.src(['./pug/**/*.pug', './pug/**/*.jade', '!**/layout*'])
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('./production/'));
+});
+gulp.task('pugI18n', function() {
   var options = {
     i18n: {
       dest: './production/',
@@ -9,7 +18,8 @@ gulp.task('pug', function() {
     },
     pretty: true
   };
-  return gulp.src(['./pug/**/*.pug', './pug/**/*.jade', '!**/layout*'])
-  .pipe(pug(options))
+  return gulp.src(['./pugI18n/**/*.pug', './pugI18n/**/*.jade', '!**/layout*'])
+  .pipe(pugI18n(options))
   .pipe(gulp.dest(options.i18n.dest));
 });
+
