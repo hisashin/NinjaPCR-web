df06d2c908c4aa9e1d32409a61c8789c50653b12
Shingo Hisakawa <shingohisakawa@gmail.com>
2018-06-10 11:57:47 +0900

    use gulp-i18n-pug istead of jquery-i18n

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,15 @@
 var gulp = require('gulp'); 
-var pug = require('gulp-pug');
+var pug = require('gulp-i18n-pug');
 
 gulp.task('pug', function() {
-  return gulp.src(['./pug/**/*.pug', './pug/**/*.jade', '!./pug/**/_*.pug'])
-  .pipe(pug({
+  var options = {
+    i18n: {
+      dest: './production/',
+      locales: './locales/*.*'
+    },
     pretty: true
-  }))
-  .pipe(gulp.dest('./production/'));
-});
-gulp.task('v1', function() {
-  return gulp.src(['./pug/v1/**/*.pug', './pug/v1/**/*.jade', '!./pug/v1/**/_*.pug'])
-  .pipe(pug({
-    pretty: true
-  }))
-  .pipe(gulp.dest('./production/'));
-});
-gulp.task('v2', function() {
-  return gulp.src(['./pug/v2/**/*.pug', './pug/v2/**/*.jade', '!./pug/v2/**/_*.pug'])
-  .pipe(pug({
-    pretty: true
-  }))
-  .pipe(gulp.dest('./production/'));
+  };
+  return gulp.src(['./pug/**/*.pug', './pug/**/*.jade', '!**/layout*'])
+  .pipe(pug(options))
+  .pipe(gulp.dest(options.i18n.dest));
 });
