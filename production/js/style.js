c1fbbe57e451998e0f98ed0fbedc86c0a3af97e8
Shingo Hisakawa <shingohisakawa@gmail.com>
2018-07-08 05:16:41 +0900

    make /contact/

--- production/js/style.js
+++ production/js/style.js
@@ -3,7 +3,6 @@ function langRedirect(lang) {
     lang = navigator.language || navigator.browserLanguage;
   //window.alert("lang=" + lang);
   var path = window.location.pathname;
-  //window.alert("path1=" + path);
   if (path.startsWith('/ja/') || path.startsWith('/en/'))
      path = path.substring(3);
   if (!path.endsWith('.html')) {
@@ -12,7 +11,6 @@ function langRedirect(lang) {
     }
     path += 'index.html';
   }
-  //window.alert("path2=" + path);
   if (lang.indexOf('ja') == 0)
     document.location.href = '/ja' + path;
   else
